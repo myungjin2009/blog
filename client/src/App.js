864c61a900e491d5845c57279ebb1813851faf31
Myeongjin Kim <myungjin2009@naver.com>
2022-07-12 17:35:46 +0900

    Header Page 생성

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import HorizonLine from './components/horizonLine';
+import HeaderMenu from './components/headerMenu/headerMenu';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="header">
+        <HorizonLine text="내생각 블로그" backColor = "cyan" />
+        <HeaderMenu></HeaderMenu>
+      </div>
+      <div className="main">
+        main!!
+      </div>
     </div>
   );
 }
---
+++ client/src/pages/MainPage/mainPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const mainPage = ({value}) => {
+    return(
+        <div>
+            이것은 mainPage에서 보내는 테스트 문장 입니다. 값은 {value} 입니다.
+        </div>
+    );
+}
+
+export default mainPage;
